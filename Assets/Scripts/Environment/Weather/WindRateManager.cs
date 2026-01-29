9d175bfe62529a18a4e86f12c8853de40e4b1e23
Grims3ver <aarden.muller@gmail.com>
2026-01-28 22:32:25 -0500

    Added Precipitation
    Added precipitation.

--- Assets/Scripts/Environment/Weather/WindRateManager.cs
+++ Assets/Scripts/Environment/Weather/WindRateManager.cs
@@ -16,9 +16,30 @@ public class SetWindRate : MonoBehaviour
     {
         foreach (var particle in particleSystems)
         {
-            var emission = particle.emission;
-            emission.rateOverTime = windyRate;
+            if (particle.gameObject.tag == "Wind")
+            {
+                var emission = particle.emission;
+                //to do: when you add a UI slide for "windiness" set the visual to also
+                //edit windyrate and rate over time and speed
+                //it should also enable soft wind for the first half (increase speed) at 50, 
+                // active stormy wind and increase speed until 100% (under velocity)
+
+                //as wind increases, regardless, Precipitation should also increase linear X + with wind
+                emission.rateOverTime = windyRate;
+            }
         }
 
+    }
+
+    void SetStorminess(float s)
+    {
+        foreach(var particle in particleSystems)
+        {
+            if (particle.gameObject.tag == "Precipitation")
+            {
+            //todo: set a UI slider for "storminess", set visual to also adjust 
+            //linear x (make it positive to move with wind), activate Noise, increase Speed
+            }
+        }
     }
 }
